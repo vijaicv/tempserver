a41c645be02b9a81d0b81f18c93f919e4d685c8d
vijaicv <vijaicv@gmail.com>
2018-04-04 19:29:51 +0530

    repair

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/",function(req,res){
   res.sendFile(__dirname + '/html/index.html');
 })
 
-var server=app.listen(process.env.port,function(){
+var server=app.listen(process.env.PORT,function(){
   var host = server.address().address
   var port = server.address().port
   console.log("app listening at http://%s:%s", host, port)
