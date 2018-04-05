62e308a12b0ef3853d095c6ea52cd6df8c43cab6
vijaicv <vijaicv@gmail.com>
2018-04-06 02:38:39 +0530

    online database comm established

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ var app=express();
 var bodyparser=require('body-parser');
 var path=require('path');
 var db;
+var dburl="mongodb://vijaicv:ucuredme@ds113179.mlab.com:13179/youcuredme"
+var MongoClient = require('mongodb').MongoClient;
+
 
 
 app.use(bodyparser.urlencoded({
         extended: true
 }));
+
+MongoClient.connect(dburl, function (err, client) {
+        if (err) {
+                console.log("could'nt connect to database => " + err)
+        };
+        db = client.db("youcuredme");
+        console.log("conected");
+})
+
 
 app.get("/",function(req,res){
   res.sendFile(__dirname + '/html/index.html');
@@ -22,11 +34,31 @@ var server=app.listen(process.env.PORT||8081,function(){
 var io=require('socket.io').listen(server);
 io.on('connection',function(socket){
   console.log("user connected");
+
+  socket.on('joinchat', function(username){
+    console.log(username+"has joined the conversation");
+    socket.username=username;
+  });
+
+  socket.on('typing',function(user){
+    socket.broadcast.emit('typing',user);
+  })
+
   socket.on('chat message', function(msg){
-    console.log(msg);
-    socket.broadcast.emit('chat message', msg);
+      console.log(msg.username);
+      var date =new Date();
+      socket.broadcast.emit('chat message', msg);
+      msg.time=date;
+      db.collection("chat").update({community:"diacare"},{$push:{chatbox:msg}},function(err,res){
+        if(err)throw(err);
+      });
   });
+
+
+
+
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+      console.log(socket.username+'disconnected');
   });
+
 })
